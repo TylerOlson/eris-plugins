f9d40355a0cf41db0abe15762e9b0d238bc97e28
Tyler Olson <tolson519@gmail.com>
2022-08-07 22:33:45 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/TylerOlson/eris-plugins
+
+go 1.19
+
+require (
+	github.com/bwmarrin/discordgo v0.25.0
+	github.com/olympus-go/eris v0.0.0-20220807175417-7febd9c5d27d
+	github.com/rs/zerolog v1.27.0
+)
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/crypto v0.0.0-20211215165025-cf75a172585e // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+)
---
+++ ping.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"github.com/olympus-go/eris/utils"
+	"github.com/rs/zerolog/log"
+)
+
+type PingPlugin struct {
+}
+
+func Ping() PingPlugin {
+	return PingPlugin{}
+}
+
+func (p PingPlugin) Name() string {
+	return "Ping"
+}
+
+func (p PingPlugin) Description() string {
+	return "Ping"
+}
+
+func (p PingPlugin) Handlers() map[string]any {
+	handlers := make(map[string]any)
+
+	handlers["ping_handler"] = func(session *discordgo.Session, i *discordgo.InteractionCreate) {
+		switch i.Type {
+		case discordgo.InteractionApplicationCommand:
+			applicationCommandData := i.ApplicationCommandData()
+
+			if applicationCommandData.Name != "ping" {
+				return
+			}
+
+			message := ""
+
+			if len(applicationCommandData.Options) > 0 {
+				recipientID := applicationCommandData.Options[0].Value.(string)
+				recipientMessage := fmt.Sprintf("Pong! (You were pinged by <@%s>!)", utils.GetInteractionUserId(i.Interaction))
+
+				recipientUserChannel, err := session.UserChannelCreate(recipientID)
+				if err != nil {
+					log.Error().Err(err).Str("userId", recipientID).Msg("could not create DM with user")
+					return
+				}
+				_, err = session.ChannelMessageSend(recipientUserChannel.ID, recipientMessage)
+				if err != nil {
+					log.Error().Err(err).Str("userId", recipientID).Msg("could not send DM with user")
+					return
+				}
+
+				message = fmt.Sprintf("You pinged <@%s>!", recipientID)
+			} else {
+				message = "Pong!"
+			}
+
+			_ = utils.SendEphemeralInteractionResponse(session, i.Interaction, message)
+		}
+	}
+	return handlers
+}
+
+func (p PingPlugin) Commands() map[string]*discordgo.ApplicationCommand {
+	commands := make(map[string]*discordgo.ApplicationCommand)
+
+	commands["ping_command"] = &discordgo.ApplicationCommand{
+		Name:        "ping",
+		Description: "Pong!",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Name:        "user",
+				Description: "Pings the specified user",
+				Type:        discordgo.ApplicationCommandOptionUser,
+				Required:    false,
+			},
+		},
+	}
+	return commands
+}
+
+func (p PingPlugin) Intents() []discordgo.Intent {
+	return nil
+}
